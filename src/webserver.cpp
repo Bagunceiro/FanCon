f25beca651d7fe58ced87fbbbe49e9ee6442e3bd
bagunceiro <paul@acasa.org.uk>
2020-11-06 13:29:43 -0300

    webserver fixes

--- src/webserver.cpp
+++ src/webserver.cpp
@@ -38,49 +38,65 @@ tr:nth-child(even) { background-color: #dddddd;}\
   server.send(200, "text/html", temp);
 }
 
+void outputMessage()
+{
+  const unsigned int maxPageSize = 1024;
+  char buffer[64];
+  char temp[maxPageSize];
+
+  snprintf(temp, maxPageSize,
+           "<HEAD><title>Controller</title><style>\
+body {font-family:Arial, Sans-Serif; font-size: 4vw;}\
+table {font-family: arial, sans-serif; border-collapse: collapse; width: 100%%;}\
+th, td {font-size: 4vw; border: 1px solid #dddddd;white-space:nowrap; text-align: left; padding: 8px; }\
+button {font-size: 4vw}\
+tr:nth-child(even) { background-color: #dddddd;}\
+</style>\
+<meta http-equiv=\"refresh\" content=\"15;url=/\" />\
+</HEAD><BODY>\
+<H1>Controller %s</H1>\
+<BODY>\
+Resetting, please wait\
+</BODY>",
+           persistant.controllername
+           );
+  server.send(200, "text/html", temp);
+}
+
 void handlePost()
 {
+  outputMessage();
   if (server.method() == HTTP_POST)
   {
 
     for (uint8_t i = 0; i < server.args(); i++)
     {
       const String argName = server.argName(i);
-      if (argName == "ctlrname")
+      if (argName == persistant.controllername_n)
       {
         snprintf(persistant.controllername, sizeof(persistant.controllername), server.arg(i).c_str());
       }
-      else if (argName == "wifissid")
-      {
-        // HARD CODED
-        // snprintf(params.wifissid, sizeof(params.wifissid), server.arg(i).c_str());
-      }
-      else if (argName == "wifipsk")
-      {
-        // HARD CODED
-        // snprintf(params.wifipsk, sizeof(params.wifipsk), server.arg(i).c_str());
-      }
-      else if (argName == "mqtthost")
+      else if (argName == persistant.mqtthost_n)
       {
         snprintf(persistant.mqtthost, sizeof(persistant.mqtthost), server.arg(i).c_str());
       }
-      else if (argName == "mqttport")
+      else if (argName == persistant.mqttport_n)
       {
         snprintf(persistant.mqttport, sizeof(persistant.mqttport), server.arg(i).c_str());
       }
-      else if (argName == "mqttuser")
+      else if (argName == persistant.mqttuser_n)
       {
         snprintf(persistant.mqttuser, sizeof(persistant.mqttuser), server.arg(i).c_str());
       }
-      else if (argName == "mqttpwd")
+      else if (argName == persistant.mqttpwd_n)
       {
         snprintf(persistant.mqttpwd, sizeof(persistant.mqttpwd), server.arg(i).c_str());
       }
-      else if (argName == "mqttroot")
+      else if (argName == persistant.mqttroot_n)
       {
         snprintf(persistant.mqttroot, sizeof(persistant.mqttroot), server.arg(i).c_str());
       }
-      else if (argName == "mqtttopic")
+      else if (argName == persistant.mqtttopic_n)
       {
         snprintf(persistant.mqtttopic, sizeof(persistant.mqtttopic), server.arg(i).c_str());
       }
@@ -114,24 +130,24 @@ tr:nth-child(even) { background-color: #dddddd;}\
 <H1>Controller Configuration</H1>\
 <FORM method=post action=/config.update>\
 <center><table>\
-<tr><td><label for=ctlrname>Controller Name:</label></td><td><input type=text id=ctlrname name=ctlrname value=%s></td></tr>\
-<tr><td><label for=mqtthost>MQTT Broker:</label></td><td><input type=text id=mqtthost name=mqtthost value=%s></td></tr>\
-<tr><td><label for=mqttport>MQTT Port:</label></td><td><input type=text id=mqttport name=mqttport value=%s></td></tr>\
-<tr><td><label for=mqttuser>MQTT User:</label></td><td><input type=text id=mqttuser name=mqttuser value=%s></td></tr>\
-<tr><td><label for=mqttuser>MQTT Password:</label></td><td><input type=text id=mqttpwd name=mqttpwd value=%s></td></tr>\
-<tr><td><label for=mqttroot>MQTT Topic root:</label></td><td><input type=text id=mqttroot name=mqttroot value=%s></td></tr>\
-<tr><td><label for=mqtttopic>MQTT Topic:</label></td><td><input type=text id=mqtttopic name=mqtttopic value=%s></td></tr>\
+<tr><td><label for=ctlrname>Controller Name:</label></td><td><input type=text name=%s value=%s></td></tr>\
+<tr><td><label for=mqtthost>MQTT Broker:</label></td><td><input type=text name=%s value=%s></td></tr>\
+<tr><td><label for=mqttport>MQTT Port:</label></td><td><input type=text name=%s value=%s></td></tr>\
+<tr><td><label for=mqttuser>MQTT User:</label></td><td><input type=text name=%s value=%s></td></tr>\
+<tr><td><label for=mqttuser>MQTT Password:</label></td><td><input type=text name=%s value=%s></td></tr>\
+<tr><td><label for=mqttroot>MQTT Topic root:</label></td><td><input type=text name=%s value=%s></td></tr>\
+<tr><td><label for=mqtttopic>MQTT Topic:</label></td><td><input type=text name=%s value=%s></td></tr>\
 <tr><td colspan=2><input type=submit value=\"Save and Reset\"></center></td></tr>\
 </table></center>\
 </FORM>\
 </BODY>",
-           persistant.controllername,
-           persistant.mqtthost,
-           persistant.mqttport,
-           persistant.mqttuser,
-           persistant.mqttpwd,
-           persistant.mqttroot,
-           persistant.mqtttopic);
+           persistant.controllername_n, persistant.controllername,
+           persistant.mqtthost_n,       persistant.mqtthost,
+           persistant.mqttport_n,       persistant.mqttport,
+           persistant.mqttuser_n,       persistant.mqttuser,
+           persistant.mqttpwd_n,        persistant.mqttpwd,
+           persistant.mqttroot_n,       persistant.mqttroot,
+           persistant.mqtttopic_n,      persistant.mqtttopic);
   server.send(200, "text/html", temp);
 }
 
